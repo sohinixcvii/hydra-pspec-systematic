009d108148f00cb4d0aa041a7a91df2b2003f733
Sohini Dutta PhD2023 PBull <sdutta@andromeda2.jb.man.ac.uk>
2025-09-04 14:19:45 +0100

    saving after 1kruns

--- filtered_and_masked_run.py
+++ filtered_and_masked_run.py
@@ -28,13 +28,15 @@ Ntimes = 80 #60 #203
 Nfreqs = 60
 freqs = np.linspace(100., 120., 120) ##120) 
 Nfgmodes = 12
-Niter=10000
+Niter=1000
 
-op_dir = './paper_plots/masked_data'
+# op_dir = './paper_plots/masked_data'
 # op_dir = './paper_plots/filtered_data'
+# op_dir = './paper_plots/filtered_1000'
+op_dir = './paper_plots/masked_1000'
 
-print("Running Masked data case")
-# print("Running filtered data case")
+# print("Running Masked data case")
+print("Running filtered data case")
 
 # Build systematics model
 nm_list = [(10,0), (11,0), (12,0), (13,0)] #high dl fr 0
