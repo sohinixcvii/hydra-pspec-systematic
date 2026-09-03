29b6cdc585f1a9da0a56f4e145c2f624ddfa8999
Sohini Dutta <sohinidutta97@gmail.com>
2026-09-03 14:35:35 +0100

    ran truncated cases

--- sys_sampler_wrapper.py
+++ sys_sampler_wrapper.py
@@ -50,13 +50,16 @@ dummy_flag = False
 np.random.seed(11)
 
 # Output directory for sampler products
-op_dir = './paper_plots/truncated/low_dl_fr_0'
+op_dir = './paper_plots/truncated/low_dl_fr_20'
 
 # Systematics mode pairs (delay index n, fringe-rate index m)
 # Case I  : nm_list = [(3,0),  (4,0),  (5,0),  (6,0)]
 # Case II : nm_list = [(10,0), (11,0), (12,0), (13,0)]
 # Case III: nm_list = [(3,20), (4,20), (5,20), (6,20)]
-nm_list = [(5,0),  (6,0)]   # Case I - truncated
+# Truncated Case I  : nm_list = [(3,0),  (4,0)]
+# Truncated Case II : nm_list = [(5,0), (6,0)]
+# Truncated Case III: nm_list = [(3,8), (4,8)]
+nm_list = [(3,8), (4,8)]   # Case III - truncated
 
 # True systematics amplitudes
 # sys_amps_true = np.array([1. + 4j, 2. + 3j, 3. + 2j, 4. + 1j])
