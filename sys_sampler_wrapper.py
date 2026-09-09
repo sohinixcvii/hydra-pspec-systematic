413c97b526fcadeb659c86ebb6d1c2767b935670
Sohini Dutta <sohinidutta97@gmail.com>
2026-09-09 20:52:57 +0100

    saving after run

--- sys_sampler_wrapper.py
+++ sys_sampler_wrapper.py
@@ -41,7 +41,7 @@ with open('res/hydra_ascii.txt', 'r') as f:
 Ntimes   = 80
 Nfreqs   = 60
 Nfgmodes = 10
-Niter    = 250000
+Niter    = 100000
 
 # Set to True to draw EoR from a Gaussian random field;
 # False to load the Burba et al. simulated EoR.
@@ -50,7 +50,7 @@ dummy_flag = False
 np.random.seed(11)
 
 # Output directory for sampler products
-op_dir = './paper_plots/250k_run/caseiv'
+op_dir = './paper_plots/250k_run/caseiv_3xsys'
 
 # Systematics mode pairs (delay index n, fringe-rate index m)
 # Case I  : nm_list = [(3,0),  (4,0),  (5,0),  (6,0)]
@@ -66,10 +66,12 @@ nm_list = [(3,0),  (4,0),  (5,0),  (6,0), (10,0), (11,0), (12,0), (13,0), (3,20)
 # True systematics amplitudes
 # sys_amps_true = np.array([1. + 4j, 2. + 3j, 3. + 2j, 4. + 1j])
 # sys_amps_true = np.array([12. + 5j, 4. + 20j]) #truncated case
+# sys_amps_true = np.array([1. + 4j, 2. + 3j, 3. + 2j, 4. + 1j,
+#                           4. + 6j, 10. + 3j, 5. + 4j, 8. + 12j,
+#                           10. + 3j, 8. + 2j, 1. + 4j, 4. + 1j])
 sys_amps_true = np.array([1. + 4j, 2. + 3j, 3. + 2j, 4. + 1j,
-                          4. + 6j, 10. + 3j, 5. + 4j, 8. + 12j,
-                          10. + 3j, 8. + 2j, 1. + 4j, 4. + 1j])
-
+                          1. + 4j, 2. + 3j, 3. + 2j, 4. + 1j,
+                          1. + 4j, 2. + 3j, 3. + 2j, 4. + 1j,])
 # Noise power spectrum amplitude
 noise_ps_val = 0.0004
 
